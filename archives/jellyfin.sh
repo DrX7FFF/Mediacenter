3fdf42f5e06dbfcc47a7f108fd4ae543e836bec8
DrX7FFF <dubourg.v@gmail.com>
2025-02-08 11:10:37 +0100

    save

--- archives/jellyfin.sh
+++ archives/jellyfin.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+  # --net=host \
 
 docker run -d \
   --name=jellyfin \
-  --net=host \
   -e TZ=Europe/Paris \
   -e PUID=0 \
   -e PGID=0 \
+  -p 7459:7359 \
+  -p 8196:8096 \
   -v /storage/.config/dockers/jellyfin/config:/config \
   -v /storage/.config/dockers/jellyfin/cache:/cache \
   -v /media/HD1:/media:ro \
